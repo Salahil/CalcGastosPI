6534b90429617bbf7539bb5124c10f2c56dfe3ee
LuidyTT <114194079+LuidyTT@users.noreply.github.com>
2023-06-06 20:29:29 -0300

    Ver_001
    Sistema base

---
+++ ProjetoSoneCar/src/com/tp/modelos/Marca.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tp.modelos;
+
+/**
+ *
+ * @author aluno
+ */
+public class Marca {
+    int id;
+    String descricao;
+}
---
+++ ProjetoSoneCar/src/com/tp/modelos/Proprietario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tp.modelos;
+
+/**
+ *
+ * @author aluno
+ */
+public class Proprietario {
+String CPF;
+String nome;
+int telefone;
+String email;
+String CNH;
+}
