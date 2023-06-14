3515de65e8426a849815a6b3ef1a32af9cba4fb3
unknown <julioykawahata@gmail.com>
2023-06-14 16:04:43 -0300

    deletei coisas adicionais e fiz mais alguma coisa que eu não lembro mais pois é... deletei uma porrada de biblioteca de ant, JavaFX que não funcionou mesmo, e umas bibliotecas de javaFreeChart que não ia usar... mas devo dizer que essa coisa de grafico só trouxe desgosto

--- ProjetoSoneCar/src/com/tp/execucao/MainClass.java
+++ ProjetoSoneCar/src/com/tp/execucao/MainClass.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.tp.execucao;
-
+import 
 /**
  *
  * @author Julio
@@ -15,6 +15,7 @@ public class MainClass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
